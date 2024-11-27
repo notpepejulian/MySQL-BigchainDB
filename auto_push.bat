dedb36225f4c6ea20741c8e2244dda518382d820
notpepejulian <pepejulian473@gmail.com>
2024-11-27 22:05:23 +0100

    Copia de seguridad autom├ítica: 27/11/2024 22:05:23,76

--- auto_push.bat
+++ auto_push.bat
@@ -1,16 +1,18 @@
 @echo off
 
-REM Crear copia de seguridad en caché
-set CACHE_DIR=%cd%\cache
-if not exist "%CACHE_DIR%" (
-    mkdir "%CACHE_DIR%"
-)
-xcopy /E /I /Y "%cd%" "%CACHE_DIR%" >nul
+REM Cambiar a la rama de caché y hacer un respaldo
+git checkout cache
+git merge main
+git add .
+git commit -m "Copia de seguridad automática: %date% %time%"
+git push origin cache
 
-REM Subir los cambios al repositorio
+REM Volver a la rama principal y subir los cambios
+git checkout main
 git add .
 git commit -m "Commit automático: %date% %time%"
 git push origin main
 
-echo Los archivos se han subido y la copia de seguridad está en %CACHE_DIR%.
+echo Los cambios se han subido al repositorio y la caché se ha actualizado.
 pause
+
