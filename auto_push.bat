9c1e75845b07d0dfba634fe0b68bb4532f0af4ed
notpepejulian <pepejulian473@gmail.com>
2024-11-27 22:03:31 +0100

    Commit autom├ítico: 27/11/2024 22:03:31,80

--- auto_push.bat
+++ auto_push.bat
@@ -1,5 +1,16 @@
 @echo off
+
+REM Crear copia de seguridad en caché
+set CACHE_DIR=%cd%\cache
+if not exist "%CACHE_DIR%" (
+    mkdir "%CACHE_DIR%"
+)
+xcopy /E /I /Y "%cd%" "%CACHE_DIR%" >nul
+
+REM Subir los cambios al repositorio
 git add .
-git commit -m "Actualización automática: %date% %time%"
+git commit -m "Commit automático: %date% %time%"
 git push origin main
+
+echo Los archivos se han subido y la copia de seguridad está en %CACHE_DIR%.
 pause
